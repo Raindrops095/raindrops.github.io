6f29ea8b73b7749ee873507f715d4d8fb6b88aa2
Raindrops <cats090ec@gmail.com>
2025-01-04 20:08:15 -0500

    test boot up

--- website/index.php
+++ website/index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Test</title>
-</head>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+    <header>hello world</header>
+    </body>
 </html>
